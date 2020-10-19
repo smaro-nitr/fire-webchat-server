71b22e645361f5abb8c3492a88d7001238546506
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2020-10-19 10:02:30 +0530

    404 fix

--- app.js
+++ app.js
@@ -56,6 +56,9 @@ io.on("connection", (socket) => {
     clearInterval(interval);
   });
 });
-io.listen(8080);
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//     console.log(`Our app is running on port ${ PORT }`);
+// });
 
 module.exports = app;
