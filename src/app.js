617b5445ac23a75a4f0f70826be429aa5bd304dd
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2022-08-18 17:33:34 +0530

    Server Switched

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const cors = require("cors");
 const indexRouter = require("./route/index");
 
 const app = express();
-// app.use(cors({ credentials: true, origin: "https://fire-webchat.web.app" }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://fire-webchat.web.app" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
